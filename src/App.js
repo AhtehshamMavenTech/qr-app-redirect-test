b439e0d3614cc6335962da1dbf51219bb93f32fb
AhtehshamMavenTech <ahtehsham.ahmad@maventechlabs.com>
2026-03-05 12:37:06 +0530

    Multiple Qr code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { QRCodeCanvas } from 'qrcode.react'
 function App() {
   return createElement(QRCodeCanvas, {
     value: 'https://ahtehshammaventech.github.io/qr-app-redirect-test/app.html',
+    //https://ahtehshamahmad.github.io/qr-app-redirect-test/app.html?app=rajwadi
+    //https://ahtehshamahmad.github.io/qr-app-redirect-test/redirect.html?app=test
     size: 220,
   })
 }
